a7053f19215470d38e4c3a64700c8dd3f1d5346f
procode7 <kr.praveen399@gmail.com>
2020-03-14 18:23:17 +0530

    nn_basic project added

---
+++ nn_basic/README.md
@@ -0,0 +1,5 @@
+steps to build the environment for this project
+
+1. install python 3.8.2
+2. pip install -r requirements.txt
+3. python main.py
---
+++ nn_basic/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.title("Streamlit 101: An in-depth introduction")
+st.markdown("Welcome to this in-depth introduction to [...].")
+st.header("Customary quote")
+st.markdown("> I just love to go home, no matter where I am [...]")
+
